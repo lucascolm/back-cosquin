416fe4aefb8a6fe323f73fe7ad7ab6af00029dd6
lucascolm <lucaspaganelli20@gmail.com>
2023-02-16 23:37:35 -0300

    .

--- api/index.js
+++ api/index.js
@@ -6,6 +6,6 @@ const { conn } = require("./src/db.js");
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
   server.listen(PORT, () => {
-    console.log("%s listening at ", PORT); // eslint-disable-line no-console
+    console.log("listening at ", PORT); // eslint-disable-line no-console
   });
 });
