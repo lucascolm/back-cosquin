fb482b637e69e7352263213245369e927f23e1f4
lucascolm <lucaspaganelli20@gmail.com>
2023-02-17 00:19:16 -0300

    dale pa

--- api/src/app.js
+++ api/src/app.js
@@ -28,13 +28,13 @@ server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use(cors(corsOptions));
-server.use((req, res, next) => {
+//server.use((req, res, next) => {
   // res.header('Access-Control-Allow-Origin', 'https://main--phenomenal-pony-798c98.netlify.app'); // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
-});
+//   res.header('Access-Control-Allow-Credentials', 'true');
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+//   next();
+// });
 
 server.use('/', routes);
 
